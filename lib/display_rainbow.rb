7470409a0fbddc8b5e71f6931e74f5d5b3da2255
Thomas Pope <tpope0928@gmail.com>
2018-07-06 00:40:12 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,10 +3,10 @@ colors = [" R: red", "O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo
 def display_rainbow(colors)
   puts "#{0}"
   puts "#{1}"
-  puts "#{1}"
-  puts "#{1}"
-  puts "#{1}"
-  puts "#{1}"
-  puts "#{1}"
-  puts "#{1}"
+  puts "#{2}"
+  puts "#{3}"
+  puts "#{4}"
+  puts "#{5}"
+  puts "#{6}"
+  puts "#{7}"
 end
