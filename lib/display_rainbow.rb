f22c703c2b368772ceac1b50a40434322ef8bdb9
Thomas Pope <tpope0928@gmail.com>
2018-07-06 00:39:53 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,2 +1,12 @@
 # 'red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet
 colors = [" R: red", "O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo", "V: violet"]
+def display_rainbow(colors)
+  puts "#{0}"
+  puts "#{1}"
+  puts "#{1}"
+  puts "#{1}"
+  puts "#{1}"
+  puts "#{1}"
+  puts "#{1}"
+  puts "#{1}"
+end
